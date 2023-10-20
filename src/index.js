9c02d7608877db1a985abedad279b40b337749a0
Egor <sychov1991@mail.ru>
2023-10-20 21:48:06 +0600

    removed jquery

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import publicFunc from './helper.js';
 import './css/style.scss';
-//import $ from 'jquery';
 
-//S$('#header1').html('Измененный заголовок');
 publicFunc("Bob");
 console.log("Index");
